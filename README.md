d719cb8a9e3d4ab93f3422f7e672d84d0de86193
J.C. Hiatt <jchiatt@me.com>
2017-02-08 13:36:06 -0600

    Update App.js

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Headless WordPress
 
+For the full tutorial, visit: 
+
 ## TL;DR
 `npm install -g create-react-app`
-`cd frontend && npm install && npm start`
+`cd headless-wp-frontend && npm install && npm start`
 
 Then, read backend/README.md to get WordPress up and going.
--- headless-wp-frontend/src/App.js
+++ headless-wp-frontend/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
       <p><strong>Title:</strong> {movie.title.rendered}</p>
       <p><strong>Release Year:</strong> {movie.acf.release_year}</p>
       <p><strong>Rating:</strong> {movie.acf.rating}</p>
-      <div><strong>Description:</strong> {movie.acf.description}</div>
+      <div><strong>Description:</strong><div dangerouslySetInnerHTML={ {__html: movie.acf.description} } /></div>
       </div>
     });
 
